ac5f09caffeb12680f419fad2f5442918ace845a
hocgin <hocgin@gmail.com>
2022-12-08 20:43:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Chatgpt on Vercel
+
+![vercel-chatgpt](https://vercelbadge.vercel.app/api/hocgin/chatgpt)
+
+> Easily chatgpt api as a service.
+
+## Usage
+
+```bash
+curl https://chatgpt.vercel.app/api?ask=${your_ask}
+```
+
+## Example
+
+### Input
+
+```bash
+curl https://chatgpt.vercel.app/api?ask=什么是Java
+```
+
+### Output
+
+```json
+{
+  "status": 200,
+  "success": true,
+  "message": "ok",
+  "data": "Java 是一种广泛使用的编程语言，它可以用于开发各种各样的应用程序，包括移动应用、企业级应用和 Web 应用程序。Java 是一种面向对象的语言，它拥有丰富的类库和工具，可以帮助开发人员更快速地开发应用程序。Java 是跨平台的，这意味着可以在多种操作系统上运行，包括 Windows、macOS 和 Linux 等。Java 程序通常使用一个独立的 Java 虚拟机来执行，这样就可以保证程序在不同平台上的一致性"
+}
+```
+
+## Deploy your own instance
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https://github.com/hocgin/vercel-chatgpt)
+
+Vercel ENV:
+
+```env
+SESSION_TOKEN=${your_session_token}
+```
+
+## License
+
+MIT 
---
+++ api/index.ts
@@ -0,0 +1,39 @@
+import {VercelRequest, VercelResponse} from '@vercel/node'
+import {ChatGPTAPI} from 'chatgpt'
+
+require('dotenv').config();
+
+async function sendMessage(message: string) {
+    let api = new ChatGPTAPI({sessionToken: process.env.SESSION_TOKEN!});
+
+    // ensure the API is properly authenticated
+    await api.ensureAuth()
+
+    // send a message and wait for the response
+    return await api.sendMessage(message);
+}
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const ask = req.query.ask as string
+    let response: any = {
+        message: "ok",
+        status: 200,
+        success: true,
+    };
+    try {
+        response.data = await sendMessage(ask)
+    } catch (err) {
+        res.status(400)
+        res.json({status: 400, success: false, message: 'Invalid URL'})
+        return
+    }
+    res.json(response)
+}
+
+// for dev
+if (process.env.NODE_ENV === 'development') {
+    (async () => {
+        const res = await sendMessage(`什么是Java`)
+        console.log(res)
+    })();
+}
