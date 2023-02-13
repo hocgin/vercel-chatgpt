08aad3b80d0a02b11f22a2d4d0a945d28272f824
hocgin <hocgin@gmail.com>
2023-02-13 21:50:58 +0800

    add open

--- api/link/openai.ts
+++ api/link/openai.ts
@@ -8,10 +8,19 @@ export async function sendMessage(askText: string) {
     const openai = new OpenAIApi(configuration);
 
     const response = await openai.createCompletion({
-        model: "code-davinci-002",
-        prompt: `${askText}`,
+        // model: "code-davinci-002",
+        model: "text-davinci-003",
+        temperature: 0,
+        max_tokens: 150,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        prompt: `\n\"\"\"\n${askText}\n\"\"\"\n`,
+        stop: ["\"\"\""],
     });
-    let ask = response.data?.choices?.[0].text;
+    let data = response.data;
+    console.log('data', data);
+    let ask = data?.choices?.[0].text;
     return {
         ask,
         message: "ok",
